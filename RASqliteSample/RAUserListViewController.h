cc5aba885a7c59c2a81033e1a011b81906bde34c
Tobias Raatiniemi <me@thedeveloperblog.net>
2013-07-27 20:18:38 +0200

    Added the user list view controller

---
+++ RASqliteSample/RAUserListViewController.h
@@ -0,0 +1,13 @@
+//
+//  RAUserListViewController.h
+//  RASqlite
+//
+//  Created by Tobias Raatiniemi on 2013-07-27.
+//  Copyright (c) 2013 The Developer Blog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAUserListViewController : UITableViewController
+
+@end

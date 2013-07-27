e039f53b20353987c4e267fb40801b8169527bef
Tobias Raatiniemi <raatiniemi@gmail.com>
2013-07-27 19:51:19 +0200

    Initial commit

---
+++ RASqliteTests/RASqliteTests.m
@@ -0,0 +1,32 @@
+//
+//  RASqliteTests.m
+//  RASqliteTests
+//
+//  Created by Tobias Raatiniemi on 2013-07-27.
+//  Copyright (c) 2013 The Developer Blog. All rights reserved.
+//
+
+#import "RASqliteTests.h"
+
+@implementation RASqliteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RASqliteTests");
+}
+
+@end

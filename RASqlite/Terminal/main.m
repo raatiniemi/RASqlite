ac1671b44703c348033f7621baad005c6c8cbeb3
Tobias Raatiniemi <raatiniemi@gmail.com>
2013-10-27 11:00:52 +0100

    Added terminal sample target.

---
+++ RASqlite/Terminal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Terminal
+//
+//  Created by Tobias Raatiniemi on 2013-10-27.
+//  Copyright (c) 2013 Raatiniemi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool {
+	}
+    return 0;
+}

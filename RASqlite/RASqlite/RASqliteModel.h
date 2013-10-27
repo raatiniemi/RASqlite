aca9bbfda1fa4dc6244d6abe0e9c40887641245d
Tobias Raatiniemi <raatiniemi@gmail.com>
2013-10-27 19:56:16 +0100

    Documentation.

--- RASqlite/RASqlite/RASqliteModel.h
+++ RASqlite/RASqlite/RASqliteModel.h
@@ -21,20 +21,74 @@
  */
 @property (nonatomic, readwrite, strong) RASqliteError *error;
 
+/**
+ Stores the defined structure for the database tables.
+ */
 @property (nonatomic, readonly, copy) NSDictionary *structure;
 
 #pragma mark - Initialization
 
+/**
+ Initialize with the name of the database file.
+
+ @param name Name of the database file.
+
+ @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ */
 - (id)initWithName:(NSString *)name;
 
 #pragma mark - Database
 
+/**
+ Retrieves the database instance.
+
+ @return Database instance, or `nil` if none is available.
+
+ @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ */
 - (sqlite3 *)database;
 
+/**
+ Retrieve the absolute path for the database file.
+
+ @return Absolute path for database file.
+
+ @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ */
 - (NSString *)path;
 
+/**
+ Open database with flags.
+
+ @param flags Flags for how to open the database.
+
+ @return `nil` if database was successfully opened, otherwise an error object.
+
+ @author Tobias Raatiniemi <raatiniemi@gmail.com>
+
+ @note
+ This method should only be called if none default flags are required. Otherwise,
+ the `open`-method will be automatically called before performing a query, unless
+ the database is already open.
+ */
 - (RASqliteError *)openWithFlags:(int)flags;
 
+/**
+ Open database with default flags.
+
+ @return `nil` if database was successfully opened, otherwise an error object.
+
+ @author Tobias Raatiniemi <raatiniemi@gmail.com>
+
+ @note
+ This method should not be called manually. It'll be called automatically before
+ performing a query, unless the database is already open.
+
+ @par
+ The default flags are `SQLITE_OPEN_CREATE` and `SQLITE_OPEN_READWRITE`, which
+ means that if the file do not exists, it will be created. And, it's open for
+ both read and write operations.
+ */
 - (RASqliteError *)open;
 
 - (RASqliteError *)close;

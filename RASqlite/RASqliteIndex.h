bda9c714672887112d60009e822c059f3134a2fd
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-02-04 05:15:10 +0100

    Added the base for the index class.

---
+++ RASqlite/RASqliteIndex.h
@@ -0,0 +1,13 @@
+//
+//  RASqliteIndex.h
+//  RASqlite
+//
+//  Created by Tobias Raatiniemi on 2014-02-04.
+//  Copyright (c) 2014 Raatiniemi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RASqliteIndex : NSObject
+
+@end
---
+++ RASqlite/RASqliteIndex.m
@@ -0,0 +1,13 @@
+//
+//  RASqliteIndex.m
+//  RASqlite
+//
+//  Created by Tobias Raatiniemi on 2014-02-04.
+//  Copyright (c) 2014 Raatiniemi. All rights reserved.
+//
+
+#import "RASqliteIndex.h"
+
+@implementation RASqliteIndex
+
+@end

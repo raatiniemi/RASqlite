caec9bebd55156a2576c0c51bea8b642d35f8d97
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-11-14 20:05:36 +0100

    better separate binding into methods
    the binding of each of the data types should be separated into their own
    methods to improve readability and keep the right level of abstraction.

--- RASqlite/RASqliteBinder.m
+++ RASqlite/RASqliteBinder.m
@@ -26,6 +26,16 @@ typedef BOOL (*isClass)(id, SEL, Class);
 
 - (NSError *)bind;
 
+- (int)bindColumn:(id)column toIndex:(unsigned int)index;
+
+- (int)bindNullToIndex:(unsigned int)index;
+
+- (int)bindText:(id)column toIndex:(unsigned int)index;
+
+- (int)bindNumber:(id)column toIndex:(unsigned int)index;
+
+- (int)bindBlob:(id)column toIndex:(unsigned int)index;
+
 @end
 
 @implementation RASqliteBinder
@@ -51,43 +61,68 @@ typedef BOOL (*isClass)(id, SEL, Class);
 - (NSError *)bind {
     NSError *error;
 
-    int code = SQLITE_OK;
     unsigned int index = 1;
     for (id column in _columns) {
-        if (_isKindOfClass(column, _selector, [NSString class])) {
-            // Sqlite do not seem to fully support UTF-16 yet, so no need to
-            // implement support for the `sqlite3_bind_text16` functionality.
-            code = sqlite3_bind_text(*_statement, index, [column UTF8String], -1, SQLITE_TRANSIENT);
-        } else if (_isKindOfClass(column, _selector, [NSNumber class])) {
-            const char *type = [column objCType];
-            if (strcmp(type, @encode(double)) == 0 || strcmp(type, @encode(float)) == 0) {
-                // Both double and float should be bound as double.
-                code = sqlite3_bind_double(*_statement, index, [column doubleValue]);
-            } else if (strcmp(type, @encode(long)) == 0 || strcmp(type, @encode(long long)) == 0) {
-                code = sqlite3_bind_int64(*_statement, index, [column longLongValue]);
-            } else {
-                // Every data type that is not specified should be bound as an int.
-                code = sqlite3_bind_int(*_statement, index, [column intValue]);
-            }
-        } else if (_isKindOfClass(column, _selector, [NSNull class])) {
-            code = sqlite3_bind_null(*_statement, index);
-        } else {
-            unsigned int length = (unsigned int) [column length];
-            code = sqlite3_bind_blob(*_statement, index, [column bytes], length, SQLITE_TRANSIENT);
+        int code = [self bindColumn:column toIndex:index];
+        if (code == SQLITE_OK) {
+            index++;
+            continue;
         }
 
-        // Check if the binding of the column was successful.
-        if (code != SQLITE_OK) {
-            NSString *message = RASqliteSF(@"Unable to bind type `%@`.", [column class]);
-            RASqliteErrorLog(@"%@", message);
+        NSString *message = RASqliteSF(@"Unable to bind type `%@`.", [column class]);
+        RASqliteErrorLog(@"%@", message);
 
-            error = [NSError code:RASqliteErrorBind message:message];
-            break;
-        }
-        index++;
+        error = [NSError code:RASqliteErrorBind message:message];
+        break;
     }
 
     return error;
+}
+
+- (int)bindColumn:(id)column toIndex:(unsigned int)index {
+    if (_isKindOfClass(column, _selector, [NSNull class])) {
+        return [self bindNullToIndex:index];
+    }
+
+    if (_isKindOfClass(column, _selector, [NSString class])) {
+        return [self bindText:column toIndex:index];
+    }
+
+    if (_isKindOfClass(column, _selector, [NSNumber class])) {
+        return [self bindNumber:column toIndex:index];
+    }
+
+    return [self bindBlob:column toIndex:index];
+}
+
+- (int)bindNullToIndex:(unsigned int)index {
+    return sqlite3_bind_null(*_statement, index);
+}
+
+- (int)bindText:(id)column toIndex:(unsigned int)index {
+    // Sqlite do not seem to fully support UTF-16 yet, so no need to
+    // implement support for the `sqlite3_bind_text16` functionality.
+    return sqlite3_bind_text(*_statement, index, [column UTF8String], -1, SQLITE_TRANSIENT);
+}
+
+- (int)bindNumber:(id)column toIndex:(unsigned int)index {
+    const char *type = [column objCType];
+
+    if (strcmp(type, @encode(double)) == 0 || strcmp(type, @encode(float)) == 0) {
+        return sqlite3_bind_double(*_statement, index, [column doubleValue]);
+    }
+
+    if (strcmp(type, @encode(long)) == 0 || strcmp(type, @encode(long long)) == 0) {
+        return sqlite3_bind_int64(*_statement, index, [column longLongValue]);
+    }
+
+    return sqlite3_bind_int(*_statement, index, [column intValue]);
+}
+
+- (int)bindBlob:(id)column toIndex:(unsigned int)index {
+    unsigned int length = (unsigned int) [column length];
+
+    return sqlite3_bind_blob(*_statement, index, [column bytes], length, SQLITE_TRANSIENT);
 }
 
 @end
